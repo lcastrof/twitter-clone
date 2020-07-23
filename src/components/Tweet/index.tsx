7570eeef0825d6505f5933ac68ab57cb3e3e79a8
Lucas de Castro <castro.lucas@engenharia.ufjf.br>
2020-07-23 16:58:02 -0300

    feed with tweets

---
+++ src/components/Tweet/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import {
+  Container,
+  Retweeted,
+  RocketSeatIcon,
+  Body,
+  Content,
+  Avatar,
+  Header,
+  Dot,
+  Description,
+  ImageContent,
+  Icons,
+  Status,
+  CommentIcon,
+  RetweetIcon,
+  LikeIcon,
+} from './styles';
+
+const Tweet: React.FC = () => {
+  return (
+    <Container>
+      <Retweeted>
+        <RocketSeatIcon />
+        Você retweetou
+      </Retweeted>
+
+      <Body>
+        <Avatar />
+
+        <Content>
+          <Header>
+            <strong>Rocketseat</strong>
+            <span>@rocketseat</span>
+            <Dot />
+            <time>23 de jul</time>
+          </Header>
+
+          <Description>Foguete não tem ré</Description>
+
+          <ImageContent />
+
+          <Icons>
+            <Status>
+              <CommentIcon />
+              18
+            </Status>
+            <Status>
+              <RetweetIcon />
+              18
+            </Status>
+            <Status>
+              <LikeIcon />
+              999
+            </Status>
+          </Icons>
+        </Content>
+      </Body>
+    </Container>
+  );
+};
+
+export default Tweet;
